1553b650b1336df3fff69a2951c53adfd076fd8a
Tatsuki Yamatani <yao099991@gmail.com>
2022-08-20 14:17:09 -0700

    Finished adding context

---
+++ client_side/src/util/graphql.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_POST_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
